fc5121ca3e5f889ace30dd9c84ab5356414fa918
pythoncgitc <59069227+pythoncgitc@users.noreply.github.com>
2020-03-06 16:59:17 -0500

    Create MPRLS_multiplexer.ino

---
+++ Arduino/MPRLS_multiplexer/MPRLS_multiplexer.ino
@@ -0,0 +1,73 @@
+/**
+ * TCA9548 I2CScanner.pde -- I2C bus scanner for Arduino
+ *
+ * Based on code c. 2009, Tod E. Kurt, http://todbot.com/blog/
+ *
+ */
+#include <Wire.h>
+#include "Adafruit_MPRLS.h"
+
+// You dont *need* a reset and EOC pin for most uses, so we set to -1 and don't connect
+#define RESET_PIN  -1  // set to any GPIO pin # to hard-reset on begin()
+#define EOC_PIN    -1  // set to any GPIO pin to read end-of-conversion by pin
+Adafruit_MPRLS mpr = Adafruit_MPRLS(RESET_PIN, EOC_PIN);
+
+#include "Wire.h"
+extern "C" { 
+#include "utility/twi.h"  // from Wire library, so we can do bus scanning
+}
+
+#define TCAADDR 0x70
+
+void tcaselect(uint8_t i) {
+  if (i > 7) return;
+ 
+  Wire.beginTransmission(TCAADDR);
+  Wire.write(1 << i);
+  Wire.endTransmission();  
+}
+
+
+// standard Arduino setup()
+void setup()
+{
+    Serial.begin(115200);
+    Serial.println("MPRLS Simple Test");
+    if (! mpr.begin()) {
+      Serial.println("Failed to communicate with MPRLS sensor, check wiring?");
+      while (1) {
+        delay(10);
+    }
+  }
+  Serial.println("Found MPRLS sensor");
+    while (!Serial);
+    delay(1000);
+
+    Wire.begin();
+    
+    Serial.begin(115200);
+    Serial.println("\nTCAScanner ready!");
+    
+    for (uint8_t t=0; t<8; t++) {
+      Serial.println(t);
+      tcaselect(t);
+      Serial.println("l");
+      Serial.print("TCA Port #"); Serial.println(t);
+
+      for (uint8_t addr = 0; addr<=127; addr++) {
+        if (addr == TCAADDR) continue;
+      
+        uint8_t data;
+        if (! twi_writeTo(addr, &data, 0, 1, 1)) {
+           Serial.print("Found I2C 0x");  Serial.println(addr,HEX);
+        }
+      }
+    }
+    Serial.println("\ndone");
+}
+
+void loop() 
+{
+    float pressure_hPa = mpr.readPressure();
+    Serial.print("Pressure (hPa): "); Serial.println(pressure_hPa);
+}
